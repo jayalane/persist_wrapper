088bf1315d1944ac9fc4854b52054678a0cdd56a
Chris Lane <chris@disputingtaste.com>
2015-07-15 19:53:27 -0700

    init version

---
+++ README.md
@@ -0,0 +1,10 @@
+A few tools to make it easier for me to read and write various data to
+sqlite3 DBs.  Generally, I'm using data that has the same structure
+each day, but the structure might evolve over the course of the
+project.  Generally, the data is extremely expensive to calculate, and
+I want to save all my intermediate results.
+
+Please feel free to contact me about this.
+
+chris@lane-jayasinha.com
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+'''
+For wrapper
+'''
+
+import datetime
+import os
+from setuptools import setup, find_packages
+
+
+NAME = 'Chris Lane'
+AUTHOR = 'Chris Lane'
+AUTHOR_EMAIL = 'chris@lane-jayasinha.com'
+URL = 'https://github.com/lanstin/wrapper'
+
+if __name__ == '__main__':
+    VERSION = 0.0.1
+    setup(name=NAME,
+          version=VERSION,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          url=URL,
+          packages=find_packages(exclude=('test', 'test.*')),
+          include_package_data=True)
