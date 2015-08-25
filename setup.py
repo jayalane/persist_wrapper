5ea0ff7e8dd429e92a2704c108523bbafbc4314c
Chris Lane <chlane@paypal.com>
2015-08-25 16:00:02 -0700

    syntax

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ AUTHOR_EMAIL = 'chris@lane-jayasinha.com'
 URL = 'https://github.com/lanstin/wrapper'
 
 if __name__ == '__main__':
-    VERSION = 0.0.1
+    VERSION = "0.0.1"
     setup(name=NAME,
           version=VERSION,
           author=AUTHOR,
