6f428478c7676521f3d5515f24b9e5bdad11af33
Chris Lane <chlane@paypal.com>
2015-10-19 17:31:25 -0700

    typo

--- date_data_cache/decorators.py
+++ date_data_cache/decorators.py
@@ -41,7 +41,7 @@ def memo_self_with_dates(method):
 
     @wraps(method)
     def memoized(*args):
-        args2 = [strv(v) for v in args]
+        args2 = [str(v) for v in args]
         str_args = method.__name__ + '-' + str(args[0].month) + '-' + str(args[0].day)  + '-' + str(args[0].year) + '-' + '-'.join(args2)
         try:
         # try to get the cached result
