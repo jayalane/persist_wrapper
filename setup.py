e22557b42a3a07682ea7114ae4b6e83a7632d3cb
Chris Lane <chlane@paypal.com>
2016-05-20 11:44:35 -0700

    cache requests is 90% of my work

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ AUTHOR_EMAIL = 'chris@lane-jayasinha.com'
 URL = 'https://github.com/jayalane/persist_wrapper'
 
 if __name__ == '__main__':
-    VERSION = "0.0.41"
+    VERSION = "0.0.42"
     setup(name=NAME,
           version=VERSION,
           description="Utility to cache date-based data",
